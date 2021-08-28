01e94b0dac2727aff61058233ef91a324e176d2e
har0ke <dev@ohargens.de>
2021-08-28 23:03:19 +0200

    Fix volume button key behavior

--- .config/i3status-rust/togglemute.sh
+++ .config/i3status-rust/togglemute.sh
@@ -1,4 +1,14 @@
-for SINK in `pactl list sinks short | grep RUNNING | awk '{ print $1 }'`
+
+sinks=$(pactl list sinks short | grep RUNNING | awk '{ print $1 }')
+if [ -z "$sinks" ]; then
+        sinks=$(pactl get-default-sink)
+fi
+
+echo "$sinks"
+
+
+
+for SINK in $sinks
 do
   pactl set-sink-mute $SINK toggle
 done
--- .config/i3status-rust/volumectl.sh
+++ .config/i3status-rust/volumectl.sh
@@ -1,4 +1,27 @@
-for SINK in `pactl list sinks short | grep RUNNING | awk '{ print $1 }'`
+
+
+sinks=$(pactl list sinks short | grep RUNNING | awk '{ print $1 }')
+if [ -z "$sinks" ]; then
+	sinks=$(pactl get-default-sink)
+fi
+
+echo "$sinks"
+
+
+for SINK in $sinks 
 do
-  pactl set-sink-volume $SINK $VOLUME $1
+  volume=$(echo $1 | sed "s/%//g")
+  if [[ $volume =~ "+" ]] || [[ $volume =~ "-" ]]; then
+    volume=$(($(pactl get-sink-volume 0 | grep -o -E "[0-9]+%" | head -1 | sed "s/%//g") $volume))
+    if [[ "${volume}" =~ "-" ]]; then 
+      volume="0"
+    fi
+    if [ "${#volume}" -gt "2" ]; then
+      volume="100"
+    fi
+  fi 
+  
+  echo "$volume"
+  pactl set-sink-volume $SINK $volume%
+  
 done
