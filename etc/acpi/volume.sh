bdc18145acf6d16335c25634dac26df7f97dd32d
har0ke <dev@ohargens.de>
2021-10-21 12:52:53 +0200

    Fix acpi volume ctl

--- etc/acpi/volume.sh
+++ etc/acpi/volume.sh
@@ -8,20 +8,21 @@ fi
 echo "$sinks"
 
 
-for SINK in $sinks 
+for SINK in $sinks
 do
   volume=$(echo $1 | sed "s/%//g")
   if [[ $volume =~ "+" ]] || [[ $volume =~ "-" ]]; then
-    volume=$(($(pactl get-sink-volume 0 | grep -o -E "[0-9]+%" | head -1 | sed "s/%//g") $volume))
-    if [[ "${volume}" =~ "-" ]]; then 
+    volume=$(($(pactl get-sink-volume $SINK | grep -o -E "[0-9]+%" | head -1 | sed "s/%//g") $volume))
+    echo $volume
+    if [[ "${volume}" =~ "-" ]]; then
       volume="0"
     fi
     if [ "${#volume}" -gt "2" ]; then
       volume="100"
     fi
-  fi 
-  
+  fi
+
   echo "$volume"
   pactl set-sink-volume $SINK $volume%
-  
+
 done
