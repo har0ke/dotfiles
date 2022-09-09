7c2aa492a3921900ad735fd5bbbfc123afacdeee
har0ke <dev@ohargens.de>
2022-09-09 08:58:06 +0200

    Minor install.sh cleanup

--- install.sh
+++ install.sh
@@ -1,52 +1,41 @@
+#!/usr/bin/env bash
 
+set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 run_as_user() {
     echo "Run with user $1: ${@:2}"
-    sudo -u $@
+    sudo -u "$@"
 }
 set -e
 link_with_bkp() {
     SRC=$2
     DEST=$3
     USER=$1
-    if [ -d $DEST ]; then
-        DEST=$DEST/$(basename $SRC)
+    if [ -d "$DEST" ]; then
+        DEST="$DEST/$(basename "$SRC")"
     fi
 
-    if [ ! -f $SRC ]; then
+    if [ ! -f "$SRC" ]; then
         echo "$SRC is not a file";
-        return -1;
+        return 1;
     fi
 
-    if [ -e $DEST ] || [ -L $DEST ]; then
-        if [ "$SRC" -ef "$(readlink $DEST)" ]; then
+    if [ -e "$DEST" ] || [ -L "$DEST" ]; then
+        if [ "$SRC" -ef "$(readlink "$DEST")" ]; then
             echo "$DEST is correctly linked"
             return
         fi
-        if [ -f $DEST.old ]; then
+        if [ -f "$DEST.old" ]; then
             echo "$DEST.old exists. Cannot create backup."
-            return -1;
+            return 1;
         fi
 	echo "$DEST will be moved to $DEST.old"
-        run_as_user $USER mv $DEST $DEST.old
+        run_as_user "$USER" mv "$DEST" "$DEST.old"
     fi
     echo "$DEST will now be linked."
-    run_as_user $USER ln -s $SRC $DEST
+    run_as_user "$USER" ln -s "$SRC" "$DEST"
 }
-
-ryzen=""
-
-xps15=""
-
-specific_files=""
-
-cpu_name="$(lscpu | grep "Model name" | cut -f2 -d: | xargs)"
-
-if [ "$cpu_name" == "Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz" ]; then
-    echo "It's my XPS15"
-    specific_files=$xps15
-fi
 
 terminal_only="\
     .oh-my-zsh/themes/oke.zsh-theme \
@@ -70,7 +59,7 @@ workstation="\
     etc/acpi/volume.sh:/:root \
     usr/share/X11/xorg.conf.d/40-libinput.conf:/:root"
 
-mode="$@"
+mode="$1"
 
 if [[ "$mode" =~ "all" ]]; then
    mode="workstation terminal"
