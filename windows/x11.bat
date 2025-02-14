acd14932443a6b5804076fa1adaf420e79075145
Oke Hargens <hargens@nanoscribe.com>
2025-02-14 13:45:10 +0100

    windows

--- windows/x11.bat
+++ windows/x11.bat
@@ -2,9 +2,9 @@
 
 call %userprofile%\qkomorebi.bat
 
-# -keyhook 
-START "" "C:\Program Files\VcXsrv\vcxsrv.exe" -multimonitors -wgl -nodecoration +xinerama -ac -clipboard -primary
+# -keyhook
+START "" "C:\Program Files\VcXsrv\vcxsrv.exe" -keyhook -multimonitors -wgl -nodecoration +xinerama -ac -clipboard -primary
 
-wsl --distribution Arch --user oke zsh -c "source ~/.zshrc; cd ~; i3"
+wsl --distribution Arch --user oke zsh -c "source ~/.zshrc; cd ~; dis 3; i3"
 
 call %userprofile%\komorebi.bat
